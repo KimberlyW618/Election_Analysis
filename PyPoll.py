a35d6573280bde286410418aa8bb2a473090c4f0
KimberlyW618 <kimw618@gmail.com>
2023-02-15 14:48:47 -0800

    Adding PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -9,31 +9,37 @@ import os
 file_to_load = os.path.join("/Users/kimberlywoods/School-UCSD-DS-Bootcamp/Election_Analysis/Resources/election_results.csv")
 
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("/Users/kimberlywoods/School-UCSD-DS-Bootcamp/Election_Analysis/analysis")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
-def error(i):
-    try:
-            val = next(i)
-            print(val)
-    except StopIteration:
-            print("Done")
-b = value(x)
-b = value(a)
-error(b)
-b
-error(b)
-error(b)
-error(b)
-error(b)
 
+# Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+        
+# Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
 
 
 # To do: perform analysis.
